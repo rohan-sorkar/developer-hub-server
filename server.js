8d7f4d6a575bb8f9a27a2db15fa69094fd1f316a
Rohan <mr.rohan.sorkar@gmail.com>
2023-06-08 01:08:39 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const auth = require("json-server-auth");
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = 9000;
+
+// Bind the router db to the app
+server.db = router.db;
+
+server.use(middlewares);
+
+const rules = auth.rewriter({
+    users: 640,
+    conversations: 660,
+    messages: 660,
+});
+
+server.use(rules);
+server.use(auth);
+server.use(router);
+
+server.listen(port);
+
+
